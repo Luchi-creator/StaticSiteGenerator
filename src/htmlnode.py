9167877bb6411013cededeeb5ab96a5089de2e85
Luchi-creator <ionut.grigore.luchian@gmail.com>
2025-08-23 12:53:50 +0300

    Added a new child class called LeafNode. Converts its parameters into html ready lines

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,28 +1,42 @@
 from enum import Enum
 
 class Tags(Enum):
-    P = "p"
-    A = "a"
-    H1 = "h1"
-    DIV = "div"
+    p = "p"
+    a = "a"
+    h1 = "h1"
+    div = "div"
 
 class HTMLNode:
-    def __init__(self,tags=None,value=None,children=None,props=None):
-        self.tags = tags
+    def __init__(self,tag=None,value=None,children=None,props=None):
+        self.tag = tag
         self.value = value
         self.children = children
         self.props = props
 
-    def to_html():
+    def to_html(self):
         raise NotImplementedError("Needs implementation")
     
     def props_to_html(self):
         string = ''
         if self.props != None:
             for i in self.props:
-                string += f" {i}={self.props[i]}"
+                string += f' {i}="{self.props[i]}"'
 
         return string
     
     def __repr__(self):
-        print(f"HTMLNode({self.tags.value},{self.value},{self.children},{self.props})")
+        return (f"HTMLNode({self.tag},{self.value},{self.children},{self.props})")
+
+
+class LeafNode(HTMLNode):
+    def __init__(self,tag,value,props=None):
+        super().__init__(tag,value,None,props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("The value property is none or empty")
+        elif self.tag == None:
+            return self.value
+        else:
+            my_props = self.props_to_html()
+            return f"<{self.tag}{my_props}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,24 +1,38 @@
 import unittest
 
-from htmlnode import Tags,HTMLNode
+from htmlnode import Tags,HTMLNode,LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
 
-    def test_propstohtml_one(self):
-        node = HTMLNode(Tags.A,None,None,{"href": "https://www.google.com","target": "_blank",})
-        string = node.props_to_html()
-        print(string)
+    # def test_propstohtml_one(self):
+    #     node = HTMLNode(Tags.a,None,None,{"href": "https://www.google.com","target": "_blank",})
+    #     string = node.props_to_html()
+    #     print(string)
 
-    def test_propstohtml_two(self):
-        node = HTMLNode(Tags.A,None,None,{"target": "_blank",})
-        string = node.props_to_html()
-        print(string)
+    # def test_propstohtml_two(self):
+    #     node = HTMLNode(Tags.a,None,None,{"target": "_blank",})
+    #     string = node.props_to_html()
+    #     print(string)
 
-    def test_propstohtml_three(self):
-        node = HTMLNode(Tags.A,None,None,None)
-        string = node.props_to_html()
-        print(string)
+    # def test_propstohtml_three(self):
+    #     node = HTMLNode(Tags.a,None,None,None)
+    #     string = node.props_to_html()
+    #     print(string)
 
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        print(node.to_html())
+
+    def test_leaf_to_html_two(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+        print(node.to_html())
+
+    def test_leaf_to_html_three(self):
+        node = LeafNode("div","Don t click me!", {"href": "https://www.google.com","color":"blue"})
+        #self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+        print(node.to_html())
 if __name__ == "__main__":
     unittest.main()
